0a817005ab3e51918a79d1ea9a338302ae116735
Sayani Gupta <Sayani.Gupta@monash.edu>
2021-03-10 01:24:36 +1100

    first commit

--- R/theme.R
+++ R/theme.R
@@ -3,7 +3,8 @@ theme_remark <- function() {
   theme_grey() +
     theme(
       axis.text = element_text(size = 24),
-      strip.text = element_text(size = 24, margin = margin()),
+      strip.text = element_text(size = 24,
+                                margin = margin()),
       axis.title = element_text(size = 24),
       legend.title = element_text(size = 24),
       legend.text = element_text(size = 24),
